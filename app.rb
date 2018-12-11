4cdf9452ca8882581358fc890f0b39a7097414e5
Vincent Chan <vchan313@gmail.com>
2018-12-11 19:20:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require_relative 'config/environment'
+  require_relative 'config/environment'
 
 class App < Sinatra::Base
 
@@ -13,7 +13,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get "/multiply/:num1/:num2" do
+  get "/multiply/:num1/:num2" do        
     
  @product = params[:num1].to_i * params[:num2].to_i
     "#{@product}"
